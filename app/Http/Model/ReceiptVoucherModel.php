43dbe14aefa8fea24e388f5de4e7b0a42324519d
pratheek-djit <37180269+pratheek-djit@users.noreply.github.com>
2018-06-01 12:18:15 +0530

    Receipt & Voucher

---
+++ app/Http/Model/ReceiptVoucherModel.php
@@ -0,0 +1,162 @@
+<?php
+    namespace App\Http\Model;
+
+    use Illuminate\Database\Eloquent\Model;
+    use DB;
+    use Auth;
+    use Exception;
+
+    class ReceiptVoucherModel extends Model
+    {
+        protected $tbl = "receipt_voucher";//table
+        public $pk = "receipt_voucher_id";//primary key
+        public $date_field = "date";
+        public $time_field = "time";
+		public $bid_field = "bid";
+		public $receipt_voucher_type_field = "receipt_voucher_type";
+		public $receipt_voucher_no_field = "receipt_voucher_no";
+		public $transaction_type_field = "transaction_type";
+		public $transaction_id_field = "transaction_id";
+		public $deleted_field = "deleted";
+		
+		private $row_data = array();
+		
+		function __construct()
+		{
+			
+		}
+		
+		public function required($data,$var)
+		{
+			if(!isset($data["{$var}"])) {
+				$dbg = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,2);
+				//print_r($dbg);
+				throw new Exception("{$var} value must be provided \n\nclass : {$dbg[1]["class"]}\n function: {$dbg[1]["function"]}() \n line : {$dbg[0]["line"]}");
+			}
+		}
+		
+		public function clear_row_data()
+		{
+			$this->row_data = array();
+		}
+		
+		public function set_row_data($data)
+		{
+			if(isset($data["{$this->pk}"])) {
+				$this->row_data["{$this->pk}"] = $data["{$this->pk}"];
+			}
+			if(isset($data["{$this->date_field}"])) {
+				$this->row_data["{$this->date_field}"] = $data["{$this->date_field}"];
+			}
+			if(isset($data["{$this->time_field}"])) {
+				$this->row_data["{$this->time_field}"] = $data["{$this->time_field}"];
+			}
+			if(isset($data["{$this->bid_field}"])) {
+				$this->row_data["{$this->bid_field}"] = $data["{$this->bid_field}"];
+			}
+			if(isset($data["{$this->receipt_voucher_type_field}"])) {
+				$this->row_data["{$this->receipt_voucher_type_field}"] = $data["{$this->receipt_voucher_type_field}"];
+			}
+			if(isset($data["{$this->receipt_voucher_no_field}"])) {
+				$this->row_data["{$this->receipt_voucher_no_field}"] = $data["{$this->receipt_voucher_no_field}"];
+			}
+			if(isset($data["{$this->transaction_type_field}"])) {
+				$this->row_data["{$this->transaction_type_field}"] = $data["{$this->transaction_type_field}"];
+			}
+			if(isset($data["{$this->transaction_id_field}"])) {
+				$this->row_data["{$this->transaction_id_field}"] = $data["{$this->transaction_id_field}"];
+			}
+			if(isset($data["{$this->deleted_field}"])) {
+				$this->row_data["{$this->deleted_field}"] = $data["{$this->deleted_field}"];
+			}
+			return;
+		}
+		
+		public function print_row_data()
+		{
+			print_r($this->row_data);exit();
+		}
+		
+		public function insert_row()
+		{
+			 return DB::table($this->tbl)
+				->insertGetId($this->row_data);
+		}
+		
+		public function update_row()
+		{
+			$this->required($this->row_data,"{$this->pk}");
+			$update_row_pk = $this->row_data["{$this->pk}"];
+			unset($this->row_data["{$this->pk}"]);
+			
+			if(count($this->row_data) == 0) {
+				return;
+			}
+			
+			return DB::table($this->tbl)
+				->where("{$this->tbl}.{$this->pk}",$update_row_pk)
+				->update($this->row_data);
+		}
+		
+		public function get_table($data = ["only_not_deleted" => false])
+		{
+			if($data["only_not_deleted"]) {
+				return DB::table($this->tbl)
+					->where("{$this->deleted_field}",0)
+					->get();
+			} else {
+				return DB::table($this->tbl)
+					->get();
+			}
+		}
+		
+		public function get_row($data)
+		{
+			$this->required($data,"{$this->pk}");
+			$row = DB::table($this->tbl)
+				->where("{$this->tbl}.{$this->pk}",$data["{$this->pk}"])
+				->first();
+			$ret_data = $this->parse_row_object(["row_data"=>$row]);
+			return $ret_data;
+		}
+		
+		public function get_value($data)
+		{
+			$this->required($data,"{$this->pk}");
+			$value = DB::table($this->tbl)
+				->where("{$this->tbl}.{$this->pk}",$data["{$this->pk}"])
+				->value($data["field"]);
+			return $value;
+        }
+		
+		public function parse_row_object($data)
+		{
+			$ret_data = [];
+			$row_data = (array)$data["row_data"];
+			foreach($row_data as $key_data=>$value_data) {
+				$ret_data["{$key_data}"] = $value_data;
+			}
+			return $ret_data;
+        }
+		
+		public function parse_table_objects($data)
+		{
+			$ret_data = [];
+			$i = -1;
+			foreach($data["table_data"] as $row_data_obj) {
+				$row_data_arr = (array)$row_data_obj;
+				$i++;
+				foreach($row_data_arr as $key_data => $value_data) {
+					$ret_data[$i]["{$key_data}"] = $value_data;
+				}
+			}
+			return $ret_data;
+		}
+
+		public function get_next_receipt()
+		{
+			
+		}
+        
+
+    }
