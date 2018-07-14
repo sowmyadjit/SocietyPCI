8a6723c0182e8a9a5ce6f9818a641fa3c450d62a
pratheek-djit <37180269+pratheek-djit@users.noreply.github.com>
2018-07-14 17:18:42 +0530

    FD/KCC certificate print - case insensitive comparision

--- resources/views/FdCertificate.blade.php
+++ resources/views/FdCertificate.blade.php
@@ -245,7 +245,7 @@
 						cerStat="<?php echo $FdCertiDetails->Fd_CertiPrint;?>"
 						KCC="<?php echo $FdCertiDetails->FDkcc;?>"
 						
-						if(KCC=="KCC")
+						if(KCC.toUpperCase()=="KCC")
 						{
 							$('.ori').hide();
 							$('.dup').hide();
@@ -253,19 +253,19 @@
 						}
 						else
 						{
-							if(cerStat=="NO" )
+							if(cerStat.toUpperCase()=="NO" )
 							{
 								$('.ori').show();
 								$('.dup').hide();	
 								$('.kcc').hide();
 							}
-							else if(cerStat=="YES")
+							else if(cerStat.toUpperCase()=="YES")
 							{
 								$('.ori').hide();
 								$('.dup').show();
 								$('.kcc').hide();
 							}
-							if(KCC=="KCC")
+							if(KCC.toUpperCase()=="KCC")
 							{
 								$('.ori').hide();
 								$('.dup').hide();
